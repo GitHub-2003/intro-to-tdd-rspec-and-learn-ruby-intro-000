fe122066b91fa5eb5d362c53b98ae8efc1ca63c6
MJU <marie.ju2012@outlook.com>
2020-01-13 20:03:53 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -9,6 +9,7 @@ describe "current_age_for_birth_year method" do
     puts("answer = " + answer.to_s)
 
     age_of_person = current_age_for_birth_year(birth_year)
+    puts("age of person = " + age_of_person)
     expect(age_of_person).to eq(19)
   end
 end
