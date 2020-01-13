802593da665d9093e09fa6226c3b82224a68b4de
MJU <marie.ju2012@outlook.com>
2020-01-13 19:22:54 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -2,5 +2,6 @@ require_relative "./current_age_for_birth_year.rb"
 
 puts "What year were you born?"
 birth_year = gets.to_i
+puts("input = " + birth_year)
 users_age = current_age_for_birth_year(birth_year)
 puts "You are: " + users_age + "years old!"
